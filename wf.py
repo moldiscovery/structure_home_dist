5a1cd59158415ad36e40995acfd5a9c439d3e7e7
Gabo <gabo@moldiscovery.com>
2025-11-17 15:29:18 +0100

    metadata update

--- wf.py
+++ wf.py
@@ -6,13 +6,13 @@ A sample workflow
 import structure_home
 
 
-biogps_dir = "/home/gabo/md/packages/BioGPS-25.01.14-rhel8"
+biogps_dir = "/home/gabo/md/build/biogps/install"
 flap_dir = "/home/gabo/md/packages/FLAP3-25.01.32-rhel8"
 
 work_dir = "/tmp/structure_home"
 
-pdb_path = "/home/gabo/md/STRUCTURE_HOME/scripttest6zdv/A2A6zdv_input.pdb"
-pdb_name = "6zdv"
+pdb_path = "/home/gabo/md/STRUCTURE_HOME/6Z10.cif"
+pdb_name = "6z10"
 
 sh = structure_home.StructureHome()
 
@@ -20,14 +20,15 @@ sh.set_flap_path(flap_dir)
 sh.set_biogps_path(biogps_dir)
 sh.set_work_dir(work_dir)
 
-sh.load_pdb(pdb_path, pdb_name)
+# sh.load_pdb(pdb_path, pdb_name)
+sh.load_cif(pdb_path, pdb_name)
 
 pockets = sh.compute_pockets(pdb_name)
 
-for pocket in pockets:
-    sh.compute_pocket_mifs(pdb_name, pocket)
+# for pocket in pockets:
+    # sh.compute_pocket_mifs(pdb_name, pocket)
 
-for pocket in pockets:
-    sh.compute_hoh(pdb_name, pocket, save_vida=True)
+# for pocket in pockets:
+    # sh.compute_hoh(pdb_name, pocket, save_vida=True)
 
 
--- wf_align.py
+++ wf_align.py
@@ -7,13 +7,13 @@ import shutil
 import structure_home
 
 
-biogps_dir = "/home/gabo/md/packages/BioGPS-25.01.24-rhel8"
+biogps_dir = "/home/gabo/md/build/biogps/install"
 flap_dir = "/home/gabo/md/packages/FLAP3-25.01.32-rhel8"
 
 work_dir = "/tmp/structure_home"
 
-target_path = "/tmp/bench_sh/repo/8pwn.pdb"
-target_name = "5iua"
+target_path = "/home/gabo/md/STRUCTURE_HOME/6X18.pdb"
+target_name = "6x18"
 
 sh = structure_home.StructureHome()
 
@@ -21,10 +21,10 @@ sh.set_flap_path(flap_dir)
 sh.set_biogps_path(biogps_dir)
 sh.set_work_dir(work_dir)
 
-rt_path = "/home/gabo/md/STRUCTURE_HOME/6zdv.pdb"
-rt_name = "6zdv"
+rt_path = "/home/gabo/md/STRUCTURE_HOME/5VEW.pdb"
+rt_name = "5vew"
 
-# shutil.rmtree(work_dir, ignore_errors=True)
+shutil.rmtree(work_dir, ignore_errors=True)
 
 if not sh.protein_avail(target_name):
     sh.load_pdb(target_path, target_name)
@@ -34,9 +34,8 @@ if not sh.protein_avail(rt_name):
 
 # compute some data on the structure that will be aligned
 
-# pockets = sh.compute_pockets(rt_name)
+pockets = sh.compute_pockets(rt_name)
 
-# sh.compute_hoh(rt_name, '001_7utz')
 # sh.compute_hoh(rt_name, pockets[0])
 
 # for pocket in pockets:
@@ -48,5 +47,5 @@ rmsd = sh.align(rt_name, target_name)
 
 print("Alignment RMSD = ", rmsd)
 
-for p in sh.protein_pockets(rt_name):
-    sh.compute_rt_pocket_mifs(rt_name, target_name, p)
+# for p in sh.protein_pockets(rt_name):
+    # sh.compute_rt_pocket_mifs(rt_name, target_name, p)
