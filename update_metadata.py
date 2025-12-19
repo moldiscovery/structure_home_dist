4cbff60e7d46a6e86af7e4c37b264a8c274d041e
Gabo <gabo@moldiscovery.com>
2025-12-19 12:40:43 +0100

    now the Structure Home API requires custom GPCR tables to provide the code of the GPCR chain, too

--- update_metadata.py
+++ update_metadata.py
@@ -1,8 +1,10 @@
 import structure_home
 
-# incremental metadata update. Add rows to the GPCR metadata CSV table and relaunch.
-metadata = "Updated_List_PDB_structures_20251121.csv"
 db_path = "metadata.db"
+
+# Incremental metadata update.
+# Add rows to the GPCR metadata CSV table and relaunch.
+metadata = "Updated_List_PDB_structures_20251121.csv"
 custom_mapping = "custom_mapping.csv"
 structure_home.update_metadata(
     db_path,
@@ -10,8 +12,8 @@ structure_home.update_metadata(
     custom_mapping,
     incremental=True)
 
-# incremental insertion of custom structures
-internal_data = "Custom_GPCR_table.csv"
+# Incremental insertion of custom structures
+internal_data = "internal_GPCR_table.csv"
 internal_custom_mapping = "internal_custom_mapping.csv"
 structure_home.add_internal_codes(
     db_path,
